beb2fbe3328b62a5727be866124b6804328a28ca
Chasbott182 <chasbott182@gmail.com>
2021-07-15 14:48:33 -0700

    added port

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ server.get("/destinations", (req, res)=>{
         res.send(destination);
 });
 
-server.listen(3000, ()=>{
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, ()=>{
     console.log("server listening");
 });
